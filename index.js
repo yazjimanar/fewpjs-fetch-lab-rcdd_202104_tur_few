5c0940a389f97c52d39c40027de9b3eac2b7e2f0
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-20 12:03:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
+function fetchBooks() {
+  // To pass the tests, don't forget to return your fetch!
 
-  function fetchBooks() {
-      return fetch('https://anapioficeandfire.com/api/books')
-    .then(resp => resp.json())
-    .then(json => renderBooks(json));
-  }
+}
+    const api = "https://anapioficeandfire.com/api/books";
+  return fetch(api)
+        .then(resp => resp.json())
+        .then(json => renderBooks(json));
+};
 
 function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
